c2836b7b0de7d8fff112c98f744a915d44ff6674
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2022-10-30 14:57:50 +0800

    Update 2022-10-30 1448H | v0.2.0 - Updated production master docker-compose file to include [jellyfin, nginx, samba, guacamole, heimdall, ipsec vpn server implementation by hwdsl2, nginx proxy manager etc] - Added nginx config folder to production - Deleted .pw from samba production as the user management is now based on the user_mgmt.sh script - Updated samba production Dockerfile to use alpine as base distribution, as well as to use user_mgmt.sh for User Management setup - Updated samba production smb.conf file to include a Out-of-the-Box (OOTB) usable example - Created a user_mgmt.sh shellscript for samba Dockerfile User setup - Added new service template - guacamole - Added new service template - heimdall - Added VPN environment file for hwdsl2 IPSec VPN Server implementation - Added new service template - nginx-proxy-manager - Added new service template - nginx reverse proxy - Created dockerfile for samba service - Updated docker-compose.yaml in samba file server service template folder : Changed dependency paths - Removed .pw from samba service template as the user management is now based on the user_mgmt.sh script - Updated samba service template smb.conf file to include an Out-of-the-Box (OOTB) usable example - Added user_mgmt.sh shellscript for samba Dockerfile User setup to samba service template

---
+++ services/samba-file-server/user_mgmt.sh
@@ -0,0 +1,23 @@
+: "
+User Management for Alpine Linux 
+"
+
+declare -A users=(
+    # [Username]="Password"
+    [admin]=Password
+    [asura]=@rchsur@
+)
+
+for username in "${!users[@]}"; do
+    password="${users[$username]}"
+    # Create User
+    adduser -H -D "$username"
+
+    # Assign Password
+    # Command is repeated twice and grouped together because 
+    # passwd and smbpasswd requires repeated input for password validation
+    echo -e "Password for [$username] : $password"
+    (echo -e "$password"; echo -e "$password") | passwd $username
+    echo -e "SMB Password for [$username] : $password"
+    (echo -e "$password"; echo -e "$password") | smbpasswd -a "$username"
+done
